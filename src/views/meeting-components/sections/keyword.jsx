a0f6928ed3410bdb775dd4fedb0afa380ed90f37
harii-in <charin1130@naver.com>
2024-01-09 16:26:46 +0900

    Design: font 적용 및 style 수정

--- src/views/meeting-components/sections/keyword.jsx
+++ src/views/meeting-components/sections/keyword.jsx
@@ -2,44 +2,47 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Col, Row, Table } from "reactstrap";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faHashtag} from "@fortawesome/free-solid-svg-icons";
+
 const KeywordComponent = ({ keywordData }) => {
     // 키워드 데이터 상태 설정
-    const [keywordDataState, setKeywordDataState] = useState({});
-    // const [keywordDataState, setKeywordDataState] = useState({
-    //     '회의': [
-    //       {
-    //         'keyword': '회의',
-    //         'title': ['정부, 국무회의서 쌍특검법 거부권 행사안 의결', '국무회의서 ‘쌍특검법’ 거부권 건의 의결', '정부, 임시국무회의 열어 ‘쌍특검법안’ 재의요구안 의결'],
-    //         'link': ['https://n.news.naver.com/mnews/article/023/0003808964?sid=100', 'https://n.news.naver.com/mnews/article/020/0003541007?sid=100', 'https://n.news.naver.com/mnews/article/056/0011636295?sid=100'],
-    //         'news_summary': '국립대학 쓰리라 연방총장들이 양곡정국 실천제 도입을 국회로 되돌려 보내자 김재준 내과 국립대학 소식검 문제와 대통령 재선 때문에 제철마저 흔들릴 게 오심 전은국  국립대학 소식검청이 일 한국라 쓰리라 소식동 쓰리라 이종욱경 제철마검속 것이라고 수검마정답란은 마멀 것만 같은 법이,말합니다.0304'
-    //       }
-    //     ],
-    //     '키워드 추출': [
-    //       {
-    //         'keyword': '키워드 추출',
-    //         'title': ['제약업계 키워 드는 AI…“비용·시간 절감” [투자360]', "K2, 내년 아웃도어 시장 키워드 'DRAGON'", "[일문일답] SKB &quot;OTT 경쟁 속 B tv 생존 키워드는 '고객'…넷플릭스 연계 상품..."],
-    //         'link': ['https://n.news.naver.com/mnews/article/016/0002245616?sid=101', 'https://n.news.naver.com/mnews/article/119/0002783640?sid=101', 'https://n.news.naver.com/mnews/article/241/0003319901?sid=105'],
-    //         'news_summary': '요약:\n식약처는 인공지능(AI) 기술을 활용한 제약 시장이 빠르게 성장하고 있으며, 2023년까지 약 1.3조원 규모로 전망되고 있다고 밝혔다. ... AI를 통해 희귀병이나 감염병 등 다양한 질환용 신약을 신속하게 개발할 수 있고, 맞춤형 신약 개발에도 용이해진다는 것이다.'
-    //       }
-    //     ],
-    //     '떡볶이': [
-    //         {
-    //           'keyword': '회의',
-    //           'title': ['정부, 국무회의서 쌍특검법 거부권 행사안 의결', '국무회의서 ‘쌍특검법’ 거부권 건의 의결', '정부, 임시국무회의 열어 ‘쌍특검법안’ 재의요구안 의결'],
-    //           'link': ['https://n.news.naver.com/mnews/article/023/0003808964?sid=100', 'https://n.news.naver.com/mnews/article/020/0003541007?sid=100', 'https://n.news.naver.com/mnews/article/056/0011636295?sid=100'],
-    //           'news_summary': '국립대학 쓰리라 연방총장들이 양곡정국 실천제 도입을 국회로 되돌려 보내자 김재준 내과 국립대학 소식검 문제와 대통령 재선 때문에 제철마저 흔들릴 게 오심 전은국  국립대학 소식검청이 일 한국라 쓰리라 소식동 쓰리라 이종욱경 제철마검속 것이라고 수검마정답란은 마멀 것만 같은 법이,말합니다.0304'
-    //         }
-    //       ],
-    //       '닭강정': [
-    //         {
-    //           'keyword': '키워드 추출',
-    //           'title': ['제약업계 키워 드는 AI…“비용·시간 절감” [투자360]', "K2, 내년 아웃도어 시장 키워드 'DRAGON'", "[일문일답] SKB &quot;OTT 경쟁 속 B tv 생존 키워드는 '고객'…넷플릭스 연계 상품..."],
-    //           'link': ['https://n.news.naver.com/mnews/article/016/0002245616?sid=101', 'https://n.news.naver.com/mnews/article/119/0002783640?sid=101', 'https://n.news.naver.com/mnews/article/241/0003319901?sid=105'],
-    //           'news_summary': '요약:\n식약처는 인공지능(AI) 기술을 활용한 제약 시장이 빠르게 성장하고 있으며, 2023년까지 약 1.3조원 규모로 전망되고 있다고 밝혔다. ... AI를 통해 희귀병이나 감염병 등 다양한 질환용 신약을 신속하게 개발할 수 있고, 맞춤형 신약 개발에도 용이해진다는 것이다.'
-    //         }
-    //       ],
+    // const [keywordDataState, setKeywordDataState] = useState({});
+    const [keywordDataState, setKeywordDataState] = useState({
+        '회의': [
+          {
+            'keyword': '회의',
+            'title': ['정부, 국무회의서 쌍특검법 거부권 행사안 의결', '국무회의서 ‘쌍특검법’ 거부권 건의 의결', '정부, 임시국무회의 열어 ‘쌍특검법안’ 재의요구안 의결'],
+            'link': ['https://n.news.naver.com/mnews/article/023/0003808964?sid=100', 'https://n.news.naver.com/mnews/article/020/0003541007?sid=100', 'https://n.news.naver.com/mnews/article/056/0011636295?sid=100'],
+            'news_summary': '국립대학 쓰리라 연방총장들이 양곡정국 실천제 도입을 국회로 되돌려 보내자 김재준 내과 국립대학 소식검 문제와 대통령 재선 때문에 제철마저 흔들릴 게 오심 전은국  국립대학 소식검청이 일 한국라 쓰리라 소식동 쓰리라 이종욱경 제철마검속 것이라고 수검마정답란은 마멀 것만 같은 법이,말합니다.0304'
+          }
+        ],
+        '키워드 추출': [
+          {
+            'keyword': '키워드 추출',
+            'title': ['제약업계 키워 드는 AI…“비용·시간 절감” [투자360]', "K2, 내년 아웃도어 시장 키워드 'DRAGON'", "[일문일답] SKB &quot;OTT 경쟁 속 B tv 생존 키워드는 '고객'…넷플릭스 연계 상품..."],
+            'link': ['https://n.news.naver.com/mnews/article/016/0002245616?sid=101', 'https://n.news.naver.com/mnews/article/119/0002783640?sid=101', 'https://n.news.naver.com/mnews/article/241/0003319901?sid=105'],
+            'news_summary': '요약:\n식약처는 인공지능(AI) 기술을 활용한 제약 시장이 빠르게 성장하고 있으며, 2023년까지 약 1.3조원 규모로 전망되고 있다고 밝혔다. ... AI를 통해 희귀병이나 감염병 등 다양한 질환용 신약을 신속하게 개발할 수 있고, 맞춤형 신약 개발에도 용이해진다는 것이다.'
+          }
+        ],
+        '떡볶이': [
+            {
+              'keyword': '회의',
+              'title': ['정부, 국무회의서 쌍특검법 거부권 행사안 의결', '국무회의서 ‘쌍특검법’ 거부권 건의 의결', '정부, 임시국무회의 열어 ‘쌍특검법안’ 재의요구안 의결'],
+              'link': ['https://n.news.naver.com/mnews/article/023/0003808964?sid=100', 'https://n.news.naver.com/mnews/article/020/0003541007?sid=100', 'https://n.news.naver.com/mnews/article/056/0011636295?sid=100'],
+              'news_summary': '국립대학 쓰리라 연방총장들이 양곡정국 실천제 도입을 국회로 되돌려 보내자 김재준 내과 국립대학 소식검 문제와 대통령 재선 때문에 제철마저 흔들릴 게 오심 전은국  국립대학 소식검청이 일 한국라 쓰리라 소식동 쓰리라 이종욱경 제철마검속 것이라고 수검마정답란은 마멀 것만 같은 법이,말합니다.0304'
+            }
+          ],
+          '닭강정': [
+            {
+              'keyword': '키워드 추출',
+              'title': ['제약업계 키워 드는 AI…“비용·시간 절감” [투자360]', "K2, 내년 아웃도어 시장 키워드 'DRAGON'", "[일문일답] SKB &quot;OTT 경쟁 속 B tv 생존 키워드는 '고객'…넷플릭스 연계 상품..."],
+              'link': ['https://n.news.naver.com/mnews/article/016/0002245616?sid=101', 'https://n.news.naver.com/mnews/article/119/0002783640?sid=101', 'https://n.news.naver.com/mnews/article/241/0003319901?sid=105'],
+              'news_summary': '요약:\n식약처는 인공지능(AI) 기술을 활용한 제약 시장이 빠르게 성장하고 있으며, 2023년까지 약 1.3조원 규모로 전망되고 있다고 밝혔다. ... AI를 통해 희귀병이나 감염병 등 다양한 질환용 신약을 신속하게 개발할 수 있고, 맞춤형 신약 개발에도 용이해진다는 것이다.'
+            }
+          ],
           
-    //   });
+      });
     const [selectedKeyword, setSelectedKeyword] = useState(null);
 
     // keywordData가 변경될 때마다 키워드 데이터 업데이트
@@ -50,9 +53,9 @@ const KeywordComponent = ({ keywordData }) => {
             ...keywordData
         }));
     }, [keywordData]);
-
+    
     return (
-        <Container style={{ maxHeight: '500px', overflowY: 'auto' }}>
+        <Container style={{ maxHeight: '500px', overflowY: 'auto', fontFamily:'Gowun Batang'}}>
             <Row style={{marginTop: '30px'}}>
                 <Col>
                     {/* 왼쪽에 키워드 목록 표시 */}
@@ -67,10 +70,10 @@ const KeywordComponent = ({ keywordData }) => {
                                     marginRight: '20px', // 각 키워드 사이의 간격을 조절
                                     marginBottom: '10px', // 행 간의 간격을 조절
                                     fontSize: '25px', // 글씨 크기를 조절
-                                    color: 'black' // 글씨 색을 항상 검정색으로 지정
+                                    color: selectedKeyword === keyword ? '#C3F669' : 'black',
                                 }}
                             >
-                                {keyword}
+                            <FontAwesomeIcon icon={faHashtag} /> {keyword}
                             </div>
                         ))}
                     </div>
--- src/views/meeting-components/sections/meeting-summary.jsx
+++ src/views/meeting-components/sections/meeting-summary.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "reactstrap";
 
 import Loading from "./loading.jsx";
+import '../css/meeting-summary.css';
 
 const MeetingSummary = () => {
     const [loading, setLoading] = useState(true);
@@ -34,27 +35,27 @@ const MeetingSummary = () => {
     // });
 
     useEffect(() => {
-        fetch("http://127.0.0.1:8000/api/token/meetingSummary", {
+        fetch("http://127.0.0.1:8000/meeting/meetsum", {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
                 "Authorization": `Token ${window.localStorage.getItem('token')}`,
             },
         })
-            .then((res) => {
-                if (!res.ok) {
-                    throw new Error(`HTTP error! Status: ${res.status}`);
-                }
-                return res.json();
-            })
-            .then((data) => {
-                setSummary(data);
-                setLoading(false);
-                console.log("data:", data);
-            })
-            .catch((err) => {
-                console.error(err);
-            });
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error(`HTTP error! Status: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then((data) => {
+            setSummary(data);
+            setLoading(false);
+            console.log("data:", data);
+        })
+        .catch((err) => {
+            console.error(err);
+        });
     }, []);
 
     if (loading) {
@@ -62,32 +63,47 @@ const MeetingSummary = () => {
     }
 
     return (
-        <Container style={{ marginTop: '40px', marginBottom: '20px', color: 'black'}}>
-            <Row>
-                <h1 style={{ fontWeight: 'bold', marginBottom: '20px' }}>{summary["회의 제목"]}</h1>
-                
-                <p className="fs-3 fw-semibold" style={{ marginTop: '5px' }}>주요 이슈 및 진행상황:</p>
-                <ul>
-                    {summary["주요 이슈 및 진행상황"].map((issue, index) => (
-                        <li key={index}>{issue}</li>
-                    ))}
-                </ul>
-                
-                <p className="fs-3 fw-semibold" style={{ marginTop: '10px' }}>새로운 상황 및 공지사항:</p>
-                <ul>
-                    {summary["새로운 상황 및 공지사항"].map((notice, index) => (
-                        <li key={index}>{notice}</li>
-                    ))}
-                </ul>
-                
-                <p className="fs-3 fw-semibold" style={{ marginTop: '10px' }}>추가 안건:</p>
-                <ul>
-                    {summary["추가 안건"].map((agenda, index) => (
-                        <li key={index}>{agenda}</li>
-                    ))}
-                </ul>
-            </Row>
-        </Container>
+        <div id="page">
+            <Container style={{ marginTop: '40px', marginBottom: '20px', color: 'black'}}>
+                <Row>
+                    <h1 style={{ fontWeight: 'bold', marginBottom: '60px' }}>{summary["회의 제목"]}</h1>
+                    
+                    <p className="fs-3 fw-semibold" style={{ marginTop: '5px' }}>주요 이슈 및 진행상황:</p>
+                    <blockquote class="block1">
+                        <ul>
+                            {summary["주요 이슈 및 진행상황"].map((issue, index) => (
+                                <li key={index}>{issue}</li>
+                            ))}
+                        </ul>
+                    </blockquote>
+                    
+                    <p className="fs-3 fw-semibold" style={{ marginTop: '30px' }}>새로운 상황 및 공지사항:</p>
+                    <blockquote class="block1">
+                        <ul>
+                            {summary["새로운 상황 및 공지사항"].map((notice, index) => (
+                                <li key={index}>{notice}</li>
+                            ))}
+                        </ul>
+                    </blockquote>
+                    
+                    <p className="fs-3 fw-semibold" style={{ marginTop: '30px' }}>추가 안건:</p>
+                    <blockquote class="block1">
+                        <ul>
+                            {summary["추가 안건"].map((agenda, index) => (
+                                <li key={index}>{agenda}</li>
+                            ))}
+                        </ul>
+                    </blockquote>
+                </Row>
+                <hr />
+                <Row>
+                    <h2 class="block2-author">Today's Keyword</h2>
+                    <blockquote class="block2">
+                        Keyword 주세요
+                    </blockquote>
+                </Row>
+            </Container>
+        </div>
     );
 };
 
