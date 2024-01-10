d48150e72060cd99f856f955e4ffd01aebc2c960
harii-in <charin1130@naver.com>
2024-01-10 11:35:18 +0900

    Feat: 로그인 전과 후 배너 변경

--- src/views/else-components/about.jsx
+++ src/views/else-components/about.jsx
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
 // core components
-import HeaderBanner4 from "../../components/banner/banner4.jsx";
+import BeginBanner from "../../components/banner/begin-banner.jsx";
+import AfterBanner from "../../components/banner/after-banner.jsx";
 import Footer from "../../components/footer/footer.jsx";
-
-// sections for this page
-// import PortfolioComponent from "./sections/portfoliocomponent.jsx";
 import TeamComponent from "./sections/teamcomponent.jsx";
 
 const AboutComponents = () => {
+    const [isLoggedIn, setLoggedIn] = useState(false);
+
+    useEffect(() => {
+        // Check if the user is logged in based on the presence of the authentication token
+        const authToken = window.localStorage.getItem('token');
+        setLoggedIn(!!authToken); // Set isLoggedIn to true if authToken is present
+    }, []); // Run this effect only once when the component mounts
+
     return (
         <div id="main-wrapper">
             <div className="page-wrapper">
                 <div className="container-fluid">
-                    <HeaderBanner4 />
+                    {/* Conditionally render BeginBanner or AfterBanner based on login status */}
+                    {isLoggedIn ? <AfterBanner /> : <BeginBanner />}
                     <TeamComponent />
                 </div>
             </div>
